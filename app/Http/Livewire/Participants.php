cf62d1e7de2ad8287deee85b3143d1c8bf9f6059
unknown <ashuzamaheshe4@gmail.com>
2022-06-30 12:09:29 +0200

    Other commit

---
+++ app/Http/Livewire/Participants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Participants extends Component
+{
+    public function render()
+    {
+        return view('livewire.participants');
+    }
+}
