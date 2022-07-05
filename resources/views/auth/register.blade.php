9400bfbcd6f002b5314a647b48c02ec8532ec1fb
unknown <ashuzamaheshe4@gmail.com>
2022-07-05 13:48:42 +0200

    other commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,8 +7,8 @@
                 <div class="bg-login text-center">
                     <div class="bg-login-overlay"></div>
                     <div class="position-relative">
-                        <h5 class="text-white font-size-20">Free Register</h5>
-                        <p class="text-white-50 mb-0">Get your free Qovex account now</p>
+                        <h5 class="text-white font-size-20">TERME DE REFERENCE</h5>
+                        <p class="text-white-50 mb-0"></p>
                         <a href="index.html" class="logo logo-admin mt-4">
                             <img src="assets/images/logo-sm-dark.png" alt="" height="30">
                         </a>
@@ -17,24 +17,53 @@
                 <div class="card-body pt-5">
 
                     <div class="p-2">
-                        <form class="form-horizontal" action="https://themesdesign.in/qovex-node/layouts/index.html">
-
+                        <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                            @csrf
                             <div class="mb-3">
                                 <label class="form-label" for="useremail">Email</label>
-                                <input type="email" class="form-control" id="useremail"
+                                <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" id="useremail"
                                     placeholder="Enter email">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label" for="username">Username</label>
-                                <input type="text" class="form-control" id="username"
+                                <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                name="name" value="{{ old('name') }}" required autocomplete="name" id="username"
                                     placeholder="Enter username">
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label" for="userpassword">Password</label>
-                                <input type="password" class="form-control" id="userpassword"
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="new-password" id="userpassword"
                                     placeholder="Enter password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label" for="password-confirm">Confirm Password</label>
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required autocomplete="new-password" placeholder="Confirm password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
 
                             <div class="mt-4">
