e8bd3710604a745275982d7840af1ec82016e2cd
unknown <ashuzamaheshe4@gmail.com>
2022-07-05 17:27:49 +0200

    other commit

--- resources/views/layouts/_top_bar.blade.php
+++ resources/views/layouts/_top_bar.blade.php
@@ -36,14 +36,18 @@
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-2.jpg"
                             alt="Header Avatar">
-                        <span class="d-none d-xl-inline-block ms-1">Patrick</span>
+                        <span class="d-none d-xl-inline-block ms-1">{{ Auth::user()->name }}</span>
                         <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <a class="dropdown-item" href="pages-profile.html"><i
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();"><i
                                 class="bx bx-user font-size-16 align-middle me-1"></i>
-                            Profile</a>
+                            Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </div>
                 </div>
 
