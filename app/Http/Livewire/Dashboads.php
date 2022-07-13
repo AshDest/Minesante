a7753b4e8fa2d530eb70c23e22f973b9647b7a33
unknown <ashuzamaheshe4@gmail.com>
2022-07-13 13:05:00 +0200

    other commit

--- app/Http/Livewire/Dashboads.php
+++ app/Http/Livewire/Dashboads.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Agent;
+use App\Models\ReferencesTerme;
+use App\Models\Service;
 use Livewire\Component;
 
 class Dashboads extends Component
 {
+    public $agents;
+    public $termes;
+    public $services;
+
+    public function mount()
+    {
+        $this->agents = Agent::count();
+        $this->termes = ReferencesTerme::count();
+        $this->services = Service::count();
+    }
+
     public function render()
     {
         return view('livewire.dashboads');
--- resources/views/livewire/dashboads.blade.php
+++ resources/views/livewire/dashboads.blade.php
@@ -1,14 +1,14 @@
 <div class="row">
-    <div class="col-xl-6">
+    <div class="col-xl-12">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-8">
                                 <div>
-                                    <p class="text-muted fw-medium mt-1 mb-2">Orders</p>
-                                    <h4>1,368</h4>
+                                    <p class="text-muted fw-medium mt-1 mb-2">Agents</p>
+                                    <h4>{{$agents}}</h4>
                                 </div>
                             </div>
 
@@ -18,19 +18,17 @@
                                 </div>
                             </div>
                         </div>
-                        <p class="mb-0"><span class="badge badge-soft-success me-2"> 0.8% <i
-                                    class="mdi mdi-arrow-up"></i> </span> From previous period</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-8">
                                 <div>
-                                    <p class="text-muted fw-medium mt-1 mb-2">Revenue</p>
-                                    <h4>$ 32,695</h4>
+                                    <p class="text-muted fw-medium mt-1 mb-2">Services</p>
+                                    <h4>{{$services}}</h4>
                                 </div>
                             </div>
 
@@ -40,53 +38,28 @@
                                 </div>
                             </div>
                         </div>
-                        <p class="mb-0"><span class="badge badge-soft-success me-2"> 0.6% <i
-                                    class="mdi mdi-arrow-up"></i> </span> From previous period</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title mb-4">Sales Report</h4>
-                <div class="row">
-                    <div class="col-sm-7">
-                        <div>
-                            <p class="mb-2">01 Jan - 31 Jan, 2020</p>
-                            <h4>$ 27, 253</h4>
-                            <p class="mt-4 mb-0"><span class="badge badge-soft-success me-2"> 0.6%
-                                    <i class="mdi mdi-arrow-up"></i> </span> From previous period
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="mt-4 mt-sm-0">
-                            <div id="sales-report-chart" class="apex-charts"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="float-end">
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Week</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Month</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Year</a>
-                        </li>
-                    </ul>
-                </div>
-                <h4 class="card-title mb-4">Email Sent</h4>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-8">
+                                <div>
+                                    <p class="text-muted fw-medium mt-1 mb-2">Terme de Reference</p>
+                                    <h4>{{$termes}}</h4>
+                                </div>
+                            </div>
 
-                <div id="mixed-chart" class="apex-charts"></div>
+                            <div class="col-4">
+                                <div>
+                                    <div id="radial-chart-2"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
